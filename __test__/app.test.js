09f1b33481f1aa503a549cc12be8b267ed2dac1f
JohnMV13 <thejohnmiesterV@gmail.com>
2018-08-28 23:41:44 -0500

    finished most of the lab

---
+++ __test__/app.test.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const request = require('supertest');
+
+import app from '../src/app';
+import Instrument from '../src/models/paths';
+
+describe('app', () => {
+  it('responds with 404 for unknown path', ()=>{
+    return request(app)
+      .get('/404')
+      .expect(404)
+      .expect('Content-Type', 'text/html; charset=utf-8');
+  });
+
+  it('responds with HTML for /', ()=>{
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect(response =>{
+        expect(response.text[0]).toBe('<');
+      });
+  });
+
+  it('responds with HTML for /cowsay?text={message}', ()=>{
+    return request(app)
+      .get('/cowsay?text=hi')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect(response =>{
+        expect(response.text).toBeDefined();
+        expect(response.text).toMatch('<html>');
+        expect(response.text).toMatch(' hi ');
+        expect(response.text).toMatch('</html>');
+      });
+  });
+
+  it('responds with JSON for /api/cowsay?text={message}', ()=>{
+    return request(app)
+      .get('/api/cowsay?text=hi')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(response =>{
+        expect(response.body).toBeDefined();
+        expect(response.body.content).toMatch(' hi ');
+      });
+  });
+  
+  describe('api routes', () => {
+    it('can PUT to /api/v1/instruments', ()=>{
+      var instrument = new Instrument({ name: 'Trumpet', class: 'Brass', retailer: 'Reimans' });
+
+      return instrument.save()
+        .then(saved => {
+          return request(app)
+            .put(`/api/v1/instruments/${saved.id}`)
+            .send({ name: 'Trumpet', class: 'Brass', retailer: 'West Music'})
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(saved);
+        });
+    });
+    it('can POST /api/v1/instruments to create instrument', () => {
+      return request(app)
+        .post('/api/v1/instruments')
+        .send({ name: 'Trumpet', class: 'Brass', retailer: 'Reimans' })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(response => {
+          expect(response.body).toBeDefined();
+          expect(response.body.id).toBeDefined();
+          expect(response.body.name).toBe('Trumpet');
+          expect(response.body.class).toBe('Brass');
+        });
+    });
+    it('can get /api/v1/instruments/:id', () => {
+      var instrument = new Instrument({ name: 'Trumpet', class: 'Brass', retailer: 'Reimans' });
+
+      return instrument.save()
+        .then(saved => {
+          return request(app)
+            .get(`/api/v1/instruments/${saved.id}`)
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(saved);
+        });
+    });
+    it('can delete /api/notes/deleteme', () => {
+      return request(app)
+        .delete('/api/v1/instruments/deleteme')
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect({ message: `ID deleteme was deleted` });
+    });
+  });
+});
---
+++ src/routes/api.js
@@ -0,0 +1,87 @@
+'use strict';
+import express from 'express';
+const router = express.Router();
+
+export default router;
+
+import Path from '../models/paths';
+
+import cowsay from 'cowsay';
+
+router.post('/500', (req, res) => {
+  throw new Error('Test Error');
+});
+router.get('/', (req, res) => {
+  html(res, '<!DOCTYPE html><html><head><title> cowsay </title>  </head><body><header><nav><ul><li><a href="/cowsay">cowsay</a></li></ul></nav><header><main><!-- project description --></main></body></html>');
+});
+router.get('/cowsay', (req, res) =>{
+  html(res, `<!DOCTYPE html><html><head><title> cowsay </title>  </head><body><h1> cowsay </h1><pre>${cowsay.say({text: req.query.text})}</pre></body></html>`);
+});
+router.get('/api/cowsay', (req, res) =>{
+  json(res, {
+    content: cowsay.say(req.query),
+  });
+});
+router.get('/api/v1/instruments/:id', (req,res) =>{
+  return Instrument.findById(req.params.id)
+    .then(instrument => {
+      res.json(instrument);
+    });
+});
+router.post('/api/cowsay', (req, res) => {
+  json(res, {
+    message: `Hello, ${req.body.name}!`,
+  });
+});
+router.post('/api/v1/instruments', (req, res) =>{
+  if (!req.body || !req.body.name || !req.body.class || !req.body.retailer) {
+    res.send(400);
+    res.end();
+    return;
+  }
+  var newInstrument = new Instrument(req.body);
+  newInstrument.save()
+    .then(saved=>{
+      res.json(saved);
+    });
+});
+router.put('/api/v1/instruments/:id', (req,res)=>{
+  return Instrument.findById(req.params.id)
+    .then(instrument =>{
+      instrument.name = req.body.name;
+      instrument.class = req.body.class;
+      instrument.retailer = req.body.retailer;
+      res.json(instrument);
+      res.end();
+      return;
+    });
+});
+router.delete('/api/v1/instruments/:id', (req,res)=>{
+  res.json({
+    message: `ID ${req.params.id} was deleted`,
+  });
+});
+
+function html(res, content, statusCode=200, statusMessage='OK'){
+  res.statusCode = statusCode;
+  res.statusMessage = statusMessage;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(content);
+  res.end();
+}
+
+  
+function json(res, object){
+  if(object){
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(object));
+    res.end();
+  } else{
+    res.statusCode = 400;
+    res.statusMessage = 'Invalid Request';
+    res.write('{"error": "invalid request: text query required"}');
+    res.end();
+  }
+}
